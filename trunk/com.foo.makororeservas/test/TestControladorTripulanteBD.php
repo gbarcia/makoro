f193bc509ddc7d7adf34b077778cd6ec2c4855ce
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-17 20:12:54 +0000

    Controlador de personal

--- trunk/com.foo.makororeservas/test/TestControladorTripulanteBD.php
+++ trunk/com.foo.makororeservas/test/TestControladorTripulanteBD.php
@@ -28,11 +28,36 @@ $tarifa = 100;
 $controlPrueba = new controladorTripulanteBDclass();
 //$resultado = $controlPrueba->agregarPersonal($tripulante);
 //$resultado = $controlPrueba->editarPersonal($tripulante);
-$resultado = $controlPrueba->consultarTotalPagoPersonal($fechaInicio, $fechaFin, $cedula, $cargo, $tarifa);
-$row = mysql_fetch_array($resultado);
-print $row[monto];
+//$resultado = $controlPrueba->consultarTotalPagoPersonal($fechaInicio, $fechaFin, $cedula, $cargo, $tarifa);
+//$resultado = $controlPrueba->consultarTotalPagoPersonal($fechaInicio, $fechaFin, $cedula, $cargo, $sueldo);
 
+//$row = mysql_fetch_array($resultado);
+//print $row[monto];
 
-
-
+echo '<table>';
+echo '<tr>';
+echo '<th>cedula</th>';
+echo '<th>nombre</th>';
+echo '<th>apellido</th>';
+echo '<th>sitio Salida</th>';
+echo '<th>sitio Llegada</th>';
+echo '<th>tiempo</th>';
+echo '<th>matricula avion</th>';
+echo '<th>cago</th>';
+echo '</tr>';
+$resultado = $controlPrueba->consultarDetallesPagoPersonal($fechaini, $fechafin, $cedula, $cargo, $sueldo);
+    while (!($row = mysql_fetch_array($resultado))) {
+    echo '<tr>';
+    echo '<td>' . $row['cedula'] . '</td>';
+    echo '<td>' . $row['nombre'] . '</td>';
+    echo '<td>' . $row['apellido'] . '</td>';
+    echo '<td>' . $row['sitioSalida'] . '</td>';
+    echo '<td>' . $row['sitioLlegada'] . '</td>';
+    echo '<td>' . $row['tiempo'] . '</td>';
+    echo '<td>' . $row['AVION_matricula'] . '</td>';
+    echo '<td>' . $row['cargo'] . '</td>';
+    echo '</tr>';
+}
+mysql_free_result($resultado);
+echo '</table>';
 ?>
