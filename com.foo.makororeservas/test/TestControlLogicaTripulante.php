feb12fa5a8a5791387d097906e87c2945caf2197
dianitau <dianitau@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-17 23:19:58 +0000

    TestControlLogicaTripulante

---
+++ com.foo.makororeservas/test/TestControlLogicaTripulante.php
@@ -0,0 +1,12 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorTipoCargoBD.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/Tripulante.class.php';
+
+$controlBD = new ControlTripulanteLogicaclass();
+
+$tabla = $controlBD->consultarTodoPersonal();
+
+foreach ($tabla as $variable) {
+    print $variable->getCedula();
+}
+?>
