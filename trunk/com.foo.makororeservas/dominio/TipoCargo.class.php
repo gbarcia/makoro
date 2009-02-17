f1555575f01afee995550e5fa4c71094d776011e
trujillo.jonathan <trujillo.jonathan@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-17 22:00:11 +0000

    Caso de uso: Tipo de Cargo

---
+++ trunk/com.foo.makororeservas/dominio/TipoCargo.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Description of TipoCargoclass
+ * Clase para manejar los tipos de cargo diponibles para los tripulantes
+ * @author jonathan
+ */
+class TipoCargoclass {
+
+    private $id;
+
+    private $cargo;
+
+    private $descripcion;
+
+    function __construct() {
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getCargo() {
+        return $this->cargo;
+    }
+
+    public function setCargo($cargo) {
+        $this->cargo = strtoupper($cargo);
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion($descripcion) {
+        $this->descripcion = strtoupper($descripcion);
+    }
+
+}
+?>
