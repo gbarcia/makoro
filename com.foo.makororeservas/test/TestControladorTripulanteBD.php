083677da717dfceaebaad8e4d9048321b3d42a92
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-17 18:51:57 +0000

    Controlador de personal

--- com.foo.makororeservas/test/TestControladorTripulanteBD.php
+++ com.foo.makororeservas/test/TestControladorTripulanteBD.php
@@ -6,22 +6,30 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorTripulanteBD.class.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/Tripulante.class.php';
 
-$tripulante = new Tripulanteclass();
+//$tripulante = new Tripulanteclass();
+//
+//$tripulante->setCedula(111);
+//$tripulante->setNombre('VICTOR');
+//$tripulante->setApellido('Molina');
+//$tripulante->setSexo('M');
+//$tripulante->setTelefono(5554433);
+//$tripulante->setEstado('Vargas');
+//$tripulante->setCiudad('La Guaira');
+//$tripulante->setDireccion('La Guairita');
+//$tripulante->setHabilitado(true);
+//$tripulante->setCargo(1);
 
-$tripulante->setCedula(111);
-$tripulante->setNombre('VICTOR');
-$tripulante->setApellido('Molina');
-$tripulante->setSexo('M');
-$tripulante->setTelefono(5554433);
-$tripulante->setEstado('Vargas');
-$tripulante->setCiudad('La Guaira');
-$tripulante->setDireccion('La Guairita');
-$tripulante->setHabilitado(true);
-$tripulante->setCargo(1);
+$cedula = 11232444;
+$fechaInicio = '2009-02-01';
+$fechaFin = '2009-03-31';
+$cargo = 1;
+$tarifa = 100;
 
 $controlPrueba = new controladorTripulanteBDclass();
 //$resultado = $controlPrueba->agregarPersonal($tripulante);
-$resultado = $controlPrueba->editarPersonal($tripulante);
+//$resultado = $controlPrueba->editarPersonal($tripulante);
+$resultado = $controlPrueba->consultarTotalPagoPersonal($fechaInicio, $fechaFin, $cedula, $cargo, $tarifa);
+
 print $resultado;
 
 
