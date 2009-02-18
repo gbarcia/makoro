160cb6b750709ca522c87f30cc550400a2024b20
mayita.uribe <mayita.uribe@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-18 01:59:05 +0000

    Controlador TripulanteBD

--- com.foo.makororeservas/test/TestControlLogicaTripulante.php
+++ com.foo.makororeservas/test/TestControlLogicaTripulante.php
@@ -4,7 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/Tripul
 
 $controlBD = new ControlTripulanteLogicaclass();
 
-$resultado = $controlBD->consultarMontoTotal("", "", 1);
+$resultado = $controlBD->consultarMontoTotal("2009-02-01", "2009-03-31", 81212334);
 
 print $resultado;
 ?>
