eaf84362ba78e8e29aa785b57da9bfb3870190c4
dianitau <dianitau@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-21 18:08:15 +0000

    Gestion de Clientes Agencias

---
+++ com.foo.makororeservas/logica/ControlClienteAgenciaLogica.class.php
@@ -0,0 +1,110 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorClienteAgenciaBD.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/ClienteAgencia.class.php';
+/**
+ * Description of ControlClienteAgenciaLogicaclass
+ * Manejo de la logica de la gestion de los clientes agencias
+ * @author Diana Uribe
+ */
+class ControlClienteAgenciaLogicaclass {
+    private $controlBD;
+
+    function __construct() {
+        $this->controlBD = new controladorClienteAgenciaBDclass();
+    }
+
+/**
+ * Metodo para agregar un nuevo cliente agencia
+ * @param <String> $rif
+ * @param <String> $nombre
+ * @param <String> $telefono
+ * @param <String> $direccion
+ * @param <String> $estado
+ * @param <String> $ciudad
+ * @param <double> $porcentajeComision
+ * @return <boolean> resultado de la operacion
+ */
+    function nuevoClienteAgencia($rif, $nombre, $telefono, $direccion, $estado, $ciudad, $porcentajeComision){
+        $clienteAgencia = new ClienteAgenciaclass();
+        $clienteAgencia->setRif($rif);
+        $clienteAgencia->setNombre($nombre);
+        $clienteAgencia->setTelefono($telefono);
+        $clienteAgencia->setDireccion($direccion);
+        $clienteAgencia->setEstado($estado);
+        $clienteAgencia->setCiudad($ciudad);
+        $clienteAgencia->setPorcentajeComision($porcentajeComision);
+        $resultado = $this->controlBD->agregarClienteAgencia($clienteAgencia);
+
+        return ($resultado);
+    }
+
+/**
+ * Metodo para actualizar los datos del cliente agencia
+ * @param <String> $rif
+ * @param <String> $nombre
+ * @param <String> $telefono
+ * @param <String> $direccion
+ * @param <String> $estado
+ * @param <String> $ciudad
+ * @param <String> $porcentajeComision
+ * @return <boolean> resultado de la operacion
+ */
+    function actualizarTripulante($rif, $nombre, $telefono, $direccion, $estado, $ciudad, $porcentajeComision){
+        $clienteAgencia = new ClienteAgenciaclass();
+        $clienteAgencia->setRif($rif);
+        $clienteAgencia->setNombre($nombre);
+        $clienteAgencia->setTelefono($telefono);
+        $clienteAgencia->setDireccion($direccion);
+        $clienteAgencia->setEstado($estado);
+        $clienteAgencia->setCiudad($ciudad);
+        $clienteAgencia->setPorcentajeComision($porcentajeComision);
+        $resultado = $this->controlBD->editarClienteAgencia($clienteAgencia);
+        return ($resultado);
+    }
+
+/**
+ * Metodo para consultar a un cliente agencia segun la busqueda
+ * @param <String, Integer> $busqueda
+ * @return <recurso> recurso de un objeto cliente agencia
+ */
+    function busquedaClienteAgenciaRifNombre($busqueda){
+        $recurso = false;
+        $recurso = $this->controlBD->consultarClienteAgenciaRifNombre($busqueda);
+        return $recurso;
+    }
+
+/**
+ * Metodo para consultar el cliente agencia con mas vuelos
+ * @return <recurso> objeto cliente agencia
+ */
+    function busquedaClienteAgenciaConMasVuelos(){
+        $recurso = false;
+        $recurso = $this->controlBD->consultarClienteAgenciaConMasVuelos();
+        return $recurso;
+    }
+
+/**
+ * Metodo para consultar por orden descendente los clientes agencias segun la
+ * cantidad de vuelos pagados
+ * @return <Coleccion> coleccion de clientes agencias
+ */
+    function busquedaClientesAgenciasVuelosDescendente(){
+        $resultado = false;
+        $resultado = $this->controlBD->consultarClientesAgenciasVuelosDescendente();
+        return $resultado;
+    }
+
+/**
+ * Metodo para consultar los clientes agencias que faltan por pagar en
+ * un lapso de tiempo determinado
+ * @param <Date> $fechaInicio
+ * @param <Date> $fechaFin
+ * @return <Coleccion> coleccion de los clientes agencias 
+ */
+    function busquedaClientesAgenciasPorPagar($fechaInicio, $fechaFin){
+        $resultado = false;
+        $resultado = $this->controlBD->consultarClientesAgenciasPorPagar($fechaInicio, $fechaFin);
+        return $resultado;
+    }
+}
+?>
---
+++ com.foo.makororeservas/test/TestLogica/TestControlLogicaClienteAgencia.php
@@ -0,0 +1,116 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/logica/ControlClienteAgenciaLogica.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/ClienteAgencia.class.php';
+
+$controlBD = new ControlClienteAgenciaLogicaclass();
+$clienteAgenciaPrueba = new ClienteAgenciaclass();
+
+///* NUEVO CLIENTE AGENCIA */
+//$resultado = $controlBD->nuevoClienteAgencia("J-112233", "XEROX", "2233445", "Chacao", "Dtto Capital", "Caracas", 0);
+//echo $resultado;
+///*-----------------------*/
+
+
+///* EDITAR CLIENTE AGENCIA */
+//$resultado = $controlBD->nuevoClienteAgencia("J-112233", "XEROX", "2003040", "Chacaito", "Dtto Capital", "Caracas", 0.35);
+//echo $resultado;
+///*-----------------------*/
+
+
+///* BUSCAR CLIENTE AGENCIA POR RIF Y NOMBRE*/
+//echo '<table border=1>';
+//echo '<tr>';
+//echo '<th>Rif</th>';
+//echo '<th>Nombre</th>';
+//echo '</tr>';
+//$result = $controlBD->busquedaClienteAgenciaRifNombre('B');
+//while ($row = mysql_fetch_array($result)) {
+//    echo '<tr>';
+//    echo '<td>' . $row[rif]. '</td>';
+//    echo '<td>' . $row[nombre]. '</td>';
+//    echo '</tr>';
+//}
+//echo '</table>';
+///*----------------------------------------*/
+
+
+///* BUSCAR CLIENTES AGENCIAS CON MAS VUELOS */
+//echo '<table border=1>';
+//echo '<tr>';
+//echo '<th>Rif</th>';
+//echo '<th>Nombre</th>';
+//echo '<th>Telefono</th>';
+//echo '<th>Direccion</th>';
+//echo '<th>Estado</th>';
+//echo '<th>Ciudad</th>';
+//echo '<th>Porcentaje Comision</th>';
+//echo '</tr>';
+//$resultado = $controlBD->busquedaClienteAgenciaConMasVuelos();
+//while ($row = mysql_fetch_array($resultado)) {
+//    echo '<tr>';
+//    echo '<td>' . $row[rif]. '</td>';
+//    echo '<td>' . $row[nombre]. '</td>';
+//    echo '<td>' . $row[telefono]. '</td>';
+//    echo '<td>' . $row[direccion]. '</td>';
+//    echo '<td>' . $row[estado]. '</td>';
+//    echo '<td>' . $row[ciudad]. '</td>';
+//    echo '<td>' . $row[porcentajeComision]. '</td>';
+//    echo '</tr>';
+//}
+//echo '</table>';
+///*-----------------------------------------*/
+
+
+///* CONSULTAR CLIENTES AGENCIAS EN ORDEN DESCENDENTE */
+//echo '<table border=1>';
+//echo '<tr>';
+//echo '<th>Rif</th>';
+//echo '<th>Nombre</th>';
+//echo '<th>Telefono</th>';
+//echo '<th>Direccion</th>';
+//echo '<th>Estado</th>';
+//echo '<th>Ciudad</th>';
+//echo '<th>Porcentaje Comision</th>';
+//echo '</tr>';
+//$resultado = $controlBD->busquedaClientesAgenciasVuelosDescendente();
+//while ($row = mysql_fetch_array($resultado)) {
+//    echo '<tr>';
+//    echo '<td>' . $row[rif]. '</td>';
+//    echo '<td>' . $row[nombre]. '</td>';
+//    echo '<td>' . $row[telefono]. '</td>';
+//    echo '<td>' . $row[direccion]. '</td>';
+//    echo '<td>' . $row[estado]. '</td>';
+//    echo '<td>' . $row[ciudad]. '</td>';
+//    echo '<td>' . $row[porcentajeComision]. '</td>';
+//    echo '</tr>';
+//}
+//echo '</table>';
+///*--------------------------------------------------*/
+
+
+///* BUSCAR CLIENTES AGENCIAS POR PAGAR */
+//    echo '<table border=1>';
+//    echo '<tr>';
+//    echo '<th>Rif</th>';
+//    echo '<th>Nombre</th>';
+//    echo '<th>Telefono</th>';
+//    echo '<th>Direccion</th>';
+//    echo '<th>Estado</th>';
+//    echo '<th>Ciudad</th>';
+//    echo '<th>Porcentaje Comision</th>';
+//    echo '</tr>';
+//    $Coleccion = $controlBD->busquedaClientesAgenciasPorPagar('2008-01-01', '2009-12-31');
+//    while ($row = mysql_fetch_array($recursoDetalles)) {
+//        echo '<tr>';
+//        echo '<td>' . $row[rif]. '</td>';
+//        echo '<td>' . $row[nombre]. '</td>';
+//        echo '<td>' . $row[telefono]. '</td>';
+//        echo '<td>' . $row[direccion]. '</td>';
+//        echo '<td>' . $row[estado] . '</td>';
+//        echo '<td>' . $row[ciudad]. '</td>';
+//        echo '<td>' . $row[porcentajeComision]. '</td>';
+//        echo '</tr>';
+//    }
+//    echo '</table>';
+///*------------------------------------*/
+?>
