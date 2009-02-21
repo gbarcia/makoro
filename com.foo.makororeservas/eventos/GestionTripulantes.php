d1efecf59c32573bca02d1d647f7149c4a487c60
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-21 23:56:14 +0000

    Nueva clase: PagoNominaTripulacion, cambios en los controladores tipoCargoBD, TripulanteBD y TripulanteLogica

--- com.foo.makororeservas/eventos/GestionTripulantes.php
+++ com.foo.makororeservas/eventos/GestionTripulantes.php
@@ -1,5 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/logica/ControlTripulanteLogica.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/ControladorTripulanteBD.class.php';
 
 /**
  * Metodo xajax para autosugerir un tipulante
@@ -7,6 +8,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/logica/Control
  * @return <type> objeto de respuesta xjax
  */
 function autoSugerir($busqueda){
+    $activado = false;
     $objResponse = new xajaxResponse();
     $resultado = "";
     $controlLogica = new ControlTripulanteLogicaclass();
@@ -14,17 +16,18 @@ function autoSugerir($busqueda){
     $numFilas = mysql_num_rows($recurso);
     $resultado = '<table class="tabla">';
     $resultado.= '<tr class="titulo">';
-    $resultado.= '<th>Cedula</th>';
-    $resultado.= '<th>Nombre</th>';
-    $resultado.= '<th>Apellido</th>';
-    $resultado.= '<th>Sexo</th>';
-    $resultado.= '<th>Telefono</th>';
-    $resultado.= '<th>Estado</th>';
-    $resultado.= '<th>Ciudad</th>';
-    $resultado.= '<th>Direccion</th>';
-    $resultado.= '<th>Cargo</th>';
-    $resultado.= '<th>Sueldo</th>';
-    $resultado.= '<th>Habilitado</th>';
+    $resultado.= '<th>CEDULA</th>';
+    $resultado.= '<th>NOMBRE</th>';
+    $resultado.= '<th>APELLIDO</th>';
+    $resultado.= '<th>SEXO</th>';
+    $resultado.= '<th>TELEFONO</th>';
+    $resultado.= '<th>ESTADO</th>';
+    $resultado.= '<th>CIUDAD</th>';
+    $resultado.= '<th>DIRECCION</th>';
+    $resultado.= '<th>CARGO</th>';
+    $resultado.= '<th>SUELDO</th>';
+    $resultado.= '<th>HABILITADO</th>';
+    $resultado.= '<th>EDITAR</th>';
     $resultado.= '</tr>';
     if (isset($busqueda) && $busqueda != "") {
         if ($numFilas > 0){
@@ -46,6 +49,7 @@ function autoSugerir($busqueda){
                 $resultado.= '<td>' . $row[cargo]. '</td>';
                 $resultado.= '<td>' . $row[sueldo]. '</td>';
                 $resultado.= '<td>' . $row[habilitado]. '</td>';
+                $resultado.= '<td><input type="button" value="EDITAR" onclick="xajax_editar('.$row[cedula].')"/></td>';
                 $resultado.= '</tr>';
                 $color = !$color;
             }
@@ -57,7 +61,11 @@ function autoSugerir($busqueda){
     else {
         $recurso = $controlLogica->consultarTodoPersonal(TRUE);
         foreach ($recurso as $row) {
-            $resultado.= '<tr>';
+            if ($color){
+                $resultado.= '<tr class="r0">';
+            } else {
+                $resultado.= '<tr class="r1">';
+            }
             $resultado.= '<td>' . $row->getCedula(). '</td>';
             $resultado.= '<td>' . $row->getNombre(). '</td>';
             $resultado.= '<td>' . $row->getApellido(). '</td>';
@@ -66,10 +74,12 @@ function autoSugerir($busqueda){
             $resultado.= '<td>' . $row->getEstado(). '</td>';
             $resultado.= '<td>' . $row->getCiudad(). '</td>';
             $resultado.= '<td>' . $row->getDireccion(). '</td>';
-            $resultado.= '<td>' . $row->getHabilitado(). '</td>';
+            $resultado.= '<td>' . $row->getHabilitadoString(). '</td>';
             $resultado.= '<td>' . $row->getCargo(). '</td>';
             $resultado.= '<td>' . $row->getSueldo(). '</td>';
+            $resultado.='<td><input type="button" value="EDITAR" onclick="xajax_editar('.$row->getCedula().')"/></td>';
             $resultado.= '</tr>';
+            $color = !$color;
         }
     }
     $resultado.= '</table>';
@@ -87,27 +97,28 @@ function autosugerirInicio ($reg1,$tamPag) {
     $controlLogica = new ControlTripulanteLogicaclass();
     echo '<table class="tabla">';
     echo '<tr class="titulo">';
-    echo '<th>Cedula</th>';
-    echo '<th>Nombre</th>';
-    echo '<th>Apellido</th>';
-    echo '<th>Sexo</th>';
-    echo '<th>Telefono </th>';
-    echo '<th>Estado</th>';
-    echo '<th>Ciudad</th>';
-    echo '<th>Direccion</th>';
-    echo '<th>Habilitado</th>';
-    echo '<th>Cargo</th>';
-    echo '<th>Sueldo</th>';
+    echo '<th>CEDULA</th>';
+    echo '<th>NOMBRE</th>';
+    echo '<th>APELLIDO</th>';
+    echo '<th>SEXO</th>';
+    echo '<th>TELEFONO</th>';
+    echo '<th>ESTADO</th>';
+    echo '<th>CIUDAD</th>';
+    echo '<th>DIRECCION</th>';
+    echo '<th>HABIITADO</th>';
+    echo '<th>CARGO</th>';
+    echo '<th>SUELDO</th>';
+     echo '<th>EDITAR</th>';
     echo '</tr>';
     $resultado = $controlLogica->consultarTodoPersonal(TRUE);
     $tamanoArreglo = sizeof($resultado);
     $color = false;
     for ($i=$reg1; $i<min($reg1+$tamPag, $tamanoArreglo); $i++) {
         if ($color){
-                    echo '<tr class="r0">';
-                } else {
-                    echo '<tr class="r1">';
-                }
+            echo '<tr class="r0">';
+        } else {
+            echo '<tr class="r1">';
+        }
         echo '<td>' . $resultado[$i]->getCedula(). '</td>';
         echo '<td>' . $resultado[$i]->getNombre(). '</td>';
         echo '<td>' . $resultado[$i]->getApellido(). '</td>';
@@ -116,9 +127,12 @@ function autosugerirInicio ($reg1,$tamPag) {
         echo '<td>' . $resultado[$i]->getEstado(). '</td>';
         echo '<td>' . $resultado[$i]->getCiudad(). '</td>';
         echo '<td>' . $resultado[$i]->getDireccion(). '</td>';
-        echo '<td>' . $resultado[$i]->getHabilitado(). '</td>';
+        echo '<td>' . $resultado[$i]->getHabilitadoString(). '</td>';
         echo '<td>' . $resultado[$i]->getCargo(). '</td>';
         echo '<td>' . $resultado[$i]->getSueldo(). '</td>';
+        echo '<form id="formulario">';
+        echo '<td><input type="button" value="EDITAR" onclick="xajax_editar('.$resultado[$i]->getCedula().')"/></td>';
+        echo '</form>';
         echo '</tr>';
         $color = !$color;
     }
@@ -146,24 +160,24 @@ function consultarNumeroTotalPersonal () {
  * @return <type>
  */
 function paginar($actual, $total, $por_pagina, $enlace) {
-  $total_paginas = ceil($total/$por_pagina);
-  $anterior = $actual - 1;
-  $posterior = $actual + 1;
-  if ($actual>1)
+    $total_paginas = ceil($total/$por_pagina);
+    $anterior = $actual - 1;
+    $posterior = $actual + 1;
+    if ($actual>1)
     $texto = "<a href=\"$enlace$anterior\">&laquo;</a> ";
-  else
+    else
     $texto = "<b>&laquo;</b> ";
-  for ($i=1; $i<$actual; $i++)
+    for ($i=1; $i<$actual; $i++)
     $texto .= "<a href=\"$enlace$i\">$i</a> ";
-  $texto .= "<b>$actual</b> ";
-  for ($i=$actual+1; $i<=$total_paginas; $i++)
+    $texto .= "<b>$actual</b> ";
+    for ($i=$actual+1; $i<=$total_paginas; $i++)
     $texto .= "<a href=\"$enlace$i\">$i</a> ";
-  if ($actual<$total_paginas)
+    if ($actual<$total_paginas)
     $texto .= "<a href=\"$enlace$posterior\">&raquo;</a>";
-  else
+    else
     $texto .= "<b>&raquo;</b>";
-  return $texto;
-    }
+    return $texto;
+}
 
 /**
  * Funcion con xjax para consultar todo el personal inhabilitado
@@ -193,10 +207,10 @@ function inabilitado ($ina) {
         $color = false;
         foreach ($recurso as $row) {
             if ($color){
-                    $resultado.= '<tr class="r0">';
-                } else {
-                    $resultado.= '<tr class="r1">';
-                }
+                $resultado.= '<tr class="r0">';
+            } else {
+                $resultado.= '<tr class="r1">';
+            }
             $resultado.= '<td>' . $row->getCedula(). '</td>';
             $resultado.= '<td>' . $row->getNombre(). '</td>';
             $resultado.= '<td>' . $row->getApellido(). '</td>';
@@ -237,10 +251,10 @@ function inabilitado ($ina) {
         $color = false;
         foreach ($recurso as $row) {
             if ($color){
-                    $resultado.= '<tr class="r0">';
-                } else {
-                    $resultado.= '<tr class="r1">';
-                }
+                $resultado.= '<tr class="r0">';
+            } else {
+                $resultado.= '<tr class="r1">';
+            }
             $resultado.= '<td>' . $row->getCedula(). '</td>';
             $resultado.= '<td>' . $row->getNombre(). '</td>';
             $resultado.= '<td>' . $row->getApellido(). '</td>';
@@ -258,6 +272,110 @@ function inabilitado ($ina) {
         $resultado.= '</table>';
         $objResponse->addAssign("gestionTripulante", "innerHTML", $resultado);
     }
+    return $objResponse;
+}
+
+function editar($cedula) {
+    $base = new controladorTripulanteBDclass();
+    $recurso = $base->consultarPersonalCedula($cedula);
+    $row = mysql_fetch_array($recurso);
+    $resultado = '<form name="form1" method="post" action="">
+  <table cellpadding="2" cellspacing="1">
+    <tr class="titulo">
+      <td colspan="2">Agregar/Editar Tripulante</td>
+    </tr>
+    <tr class="r1">
+      <td>Cedula</td>
+      <td><label>
+        <input type="text" name="cedula" id="cedula" size="30" value='.$row[cedula].'>
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td>Nombre</td>
+      <td><label>
+        <input type="text" name="nombre" id="nombre" size="30" value='.$row[nombre].'>
+      </label></td>
+    </tr>
+    <tr class="r1">
+      <td>Apellido</td>
+      <td><label>
+        <input type="text" name="apellido" id="apellido" size="30" value="'.$row[apellido].'">
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td>Sexo</td>
+      <td><p>
+        <label>
+          <input type="radio" name="sexo" value="masculino" id="sexo_0"';
+    if($row[sexo] == 'M'){
+        $resultado.= 'checked="checked"';
+    }
+    $resultado.= '>
+          Masculino</label>
+        <br>
+        <label>
+          <input type="radio" name="sexo" value="femenino" id="sexo_1"';
+    if($row[sexo] == 'F'){
+        $resultado.= 'checked="checked"';
+    }
+    $resultado.= '>
+          Femenino</label>
+      </td>
+    </tr>
+    <tr class="r1">
+      <td>Telefono</td>
+      <td><label>
+        <input type="text" name="telefono" id="telefono" size="30" value="'.$row[telefono].'">
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td>Estado de residencia</td>
+      <td><label>
+        <input type="text" name="estado" id="estado" size="30" value="'.$row[estado].'">
+      </label></td>
+    </tr>
+    <tr class="r1">
+      <td>Ciudad de residencia</td>
+      <td><label>
+        <input type="text" name="ciudad" id="ciudad" size="30" value="'.$row[ciudad].'">
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td>Direccion de residencia</td>
+      <td><label>
+        <textarea name="direccion" id="direccion" cols="23" rows="3">'.$row[direccion].'</textarea>
+      </label></td>
+    </tr>
+    <tr class="r1">
+      <td>Cargo</td>
+      <td><label>
+        <select name="cargo" id="cargo">
+        </select>
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td>Sueldo</td>
+      <td><label>
+        <input type="text" name="sueldo" id="sueldo" size="30" value="'.$row[sueldo].'">
+      </label></td>
+    </tr>
+    <tr class="r1">
+      <td>Habilitado</td>
+      <td><label>
+        <input type="checkbox" name="habilitado" id="habilitado" size="30" value="'.$row[habilitado].'">
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td height="26" colspan="2"><label>
+          <div align="center">
+            <input type="submit" name="button" id="button" value="Submit">
+              </div>
+      </label></td>
+    </tr>
+  </table>
+</form>';
+    $objResponse = new xajaxResponse();
+    $objResponse->addAssign("izq", "innerHTML", $resultado);
     return $objResponse;
 }
 ?>
