620f1b4093558c8704c80d8bad27a48d8a122b72
dianitau <dianitau@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-02-24 04:10:07 +0000

    Gestion de Pago

--- com.foo.makororeservas/logica/ControlPagoLogica.class.php
+++ com.foo.makororeservas/logica/ControlPagoLogica.class.php
@@ -13,6 +13,62 @@ class ControlPagoLogicaclass {
         $this->controlBD = new controladorPagoBDclass();
     }
 
-    
+/**
+ * Metodo para agregar un nuevo pago
+ * @param <String> $tipo
+ * @param <double> $monto
+ * @param <String> $nombreBanco
+ * @param <Integer> $numeroTransaccion
+ * @param <Integer> $monedaId
+ * @return <boolean> resultado de la operacion 
+ */
+    function nuevoPago($tipo,$monto,$nombreBanco,$numeroTransaccion,$monedaId) {
+        $pago = new Pagoclass();
+        $pago->setTipo($tipo);
+        $pago->setMonto($monto);
+        $pago->setNombreBanco($nombreBanco);
+        $pago->setNumeroTransaccion($numeroTransaccion);
+        $pago->setMonedaId($monedaId);
+        $resultado = $this->controlBD->agregarPago($pago);
+
+        return ($resultado);
+    }
+
+/**
+ * Metodo para actulizar los datos del pago
+ * @param <Integer> $id
+ * @param <String> $tipo
+ * @param <double> $monto
+ * @param <String> $nombreBanco
+ * @param <Integer> $numeroTransaccion
+ * @param <Integer> $monedaId
+ * @return <boolean> resultado de la operacion
+ */
+    function actualizarPago($id,$tipo,$monto,$nombreBanco,$numeroTransaccion,$monedaId) {
+        $pago = new Pagoclass();
+        $pago->setId($id);
+        $pago->setTipo($tipo);
+        $pago->setMonto($monto);
+        $pago->setNombreBanco($nombreBanco);
+        $pago->setNumeroTransaccion($numeroTransaccion);
+        $pago->setMonedaId($monedaId);
+        $resultado = $this->controlBD->editarPago($pago);
+        return $resultado;
+    }
+
+/**
+ * Metodo para consultar todos los pagos
+ * @return <recurso>
+ */
+    function buscarPagos() {
+        $recurso = false;
+        $recurso = $this->controlBD->consultarPagos();
+        return $recurso;
+    }
+
+    function buscarCancelacionPagoClienteParticular($cedula, $fechaini, $fechafin) {
+        $reserva->setEstado("CA");
+        $this->controlBD->editarPago($reserva);
+    }
 }
 ?>
