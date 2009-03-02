6cc73ef690564b22e8f9442b21970894578df290
trujillo.jonathan <trujillo.jonathan@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-02 23:36:39 +0000

    GUI tripulante

---
+++ trunk/com.foo.makororeservas/gui/menu.php
@@ -0,0 +1,54 @@
+<ul id="MenuBar" class="MenuBarHorizontal">
+  <li><a href="#">Reservas</a> </li>
+  <li><a href="#" class="MenuBarItemSubmenu">Vuelos</a>
+      <ul>
+        <li><a href="#">Gestion</a></li>
+        <li><a href="#">Rutas</a></li>
+        <li><a href="#">Aviones</a></li>
+        <li><a href="#">Pasajeros</a></li>
+        <li><a href="#" class="MenuBarItemSubmenu">Personal</a>
+            <ul>
+              <li><a href="#">Gestion</a></li>
+              <li><a href="#">Pagos</a></li>
+              <li><a href="#">Cargos</a></li>
+          </ul>
+        </li>
+    </ul>
+  </li>
+  <li><a href="#" class="MenuBarItemSubmenu">Clientes</a>
+    <ul>
+      <li><a href="#" class="MenuBarItemSubmenu">Gestion</a>
+        <ul>
+          <li><a href="#">Agencias de viaje</a></li>
+          <li><a href="#">Particulares</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Pagos</a></li>
+    </ul>
+  </li>
+  <li><a href="#" class="MenuBarItemSubmenu">Sucursal</a>
+    <ul>
+      <li><a href="#">Gestion</a></li>
+      <li><a href="#">Estadisticas</a></li>
+    </ul>
+  </li>
+  <li><a href="#" class="MenuBarItemSubmenu">Configuracion</a>
+      <ul>
+        <li><a href="#">Moneda</a></li>
+        <li><a href="#">Sistema</a></li>
+      </ul>
+  </li>
+  <li><a href="#" class="MenuBarItemSubmenu">Servicio</a>
+      <ul>
+        <li><a href="#">Gestion</a></li>
+        <li><a href="#">Posadas</a></li>
+      </ul>
+  </li>
+  <li><a href="#">Ayuda</a></li>
+</ul>
+
+<script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+//-->
+</script>
