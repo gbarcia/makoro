6a1d710154b0badaf6a15633428e4ac3f35f3dde
dianitau <dianitau@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-04 21:03:08 +0000

    Gestion Boleto

---
+++ trunk/com.foo.makororeservas/dominio/ReciboDePago.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Description of ReciboDePago
+ * Clase para manejar los recibos de pago
+ * @author Diana Uribe
+ */
+class ReciboDePago {
+    private $agente;
+    private $solicitud;
+    private $fechaEmision;
+    private $fechaIda;
+    private $horaIda;
+    private $fechaVuelta;
+    private $horaVuelta;
+    private $salida;
+    private $retorno;
+    private $coleccionPasajero;
+    private $servicio;
+    private $cliente;
+    private $identificadorCliente;
+    private $cantidadAdultos;
+    private $cantidadNinos;
+}
+?>
