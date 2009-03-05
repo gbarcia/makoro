555a99f3af6a4f94cbde294016fd3e9c37ed7153
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-05 20:46:03 +0000

    Gestion Tipo Servicio

---
+++ trunk/com.foo.makororeservas/eventos/GestionAgenciaMasVuelos.php
@@ -0,0 +1,50 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/ClienteAgencia.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorClienteAgenciaBD.class.php';
+
+function cadenaAgenciaMasVuelos () {
+    $resultado = "";
+    $objResponse = new xajaxResponse();
+    $resultado = '<form id="formularioEditarMarcar">';
+    $resultado.= '<table cellspacing="0">';
+    $resultado.= '<thead>';
+    $resultado.= '<tr>';
+    $resultado.= '<th>RIF</th>';
+    $resultado.= '<th>NOMBRE</th>';
+    $resultado.= '<th>TELEFONO</th>';
+    $resultado.= '<th>ESTADO</th>';
+    $resultado.= '<th>CIUDAD</th>';
+    $resultado.= '<th>NUMERO VUELOS</th>';
+    $resultado.= '</tr>';
+    $resultado.= '</thead>';
+    $controlLogica = new controladorClienteAgenciaBDclass();
+    $recurso = $controlLogica->consultarClientesAgenciasVuelosDescendente();
+    $color = false;
+    while ($row = mysql_fetch_array($recurso)) {
+        if ($color){
+            $resultado.= '<tr class="r0">';
+        } else {
+            $resultado.= '<tr class="r1">';
+        }
+        $resultado.= '<td>' . $row[rif] .'</td>';
+        $resultado.= '<td>' . $row[nombre]. '</td>';
+        $resultado.= '<td>' . $row[telefono]. '</td>';
+        $resultado.= '<td>' . $row[estado]. '</td>';
+        $resultado.= '<td>' . $row[ciudad]. '</td>';
+        $resultado.= '<td>' . $row[cnt].  '</td>';
+        $resultado.= '</tr>';
+        $color = !$color;
+    }
+    $resultado.= '</table>';
+    $resultado.= '</form>';
+    return $resultado;
+}
+
+function inicio () {
+    $resultado = cadenaAgenciaMasVuelos();
+    $objResponse = new xajaxResponse();
+    $objResponse->addAssign("gestion", "innerHTML", $resultado);
+    return $objResponse;
+}
+
+?>
