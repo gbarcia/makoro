e3938b5bbda0a47ac480bc17b9c02c57493587f8
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-05 23:19:02 +0000

    Gestion Tipo Servicio

---
+++ com.foo.makororeservas/eventos/GestionSucursalMasVentas.php
@@ -0,0 +1,38 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorSucursalBD.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/Sucursal.class.php';
+
+function accion ($datos) {
+    $control = new controladorSucursalBDclass();
+    $recurso = $control->consultarSucursalMasVentas($datos[fechaInicio], $datos[fechaFin]);
+    $resultado = "";
+    $objResponse = new xajaxResponse();
+    $resultado = '<form id="formularioEditarMarcar">';
+    $resultado.= '<table cellspacing="0" class="formTable">';
+    $resultado.= '<thead>';
+    $resultado.= '<tr>';
+    $resultado.= '<th>NOMBRE</th>';
+    $resultado.= '<th>MONTO</th>';
+    $resultado.= '<th>MONEDA</th>';
+    $resultado.= '</tr>';
+    $resultado.= '</thead>';
+    $color = false;
+    while ($row = mysql_fetch_array($recurso)) {
+        if ($color){
+            $resultado.= '<tr class="r0">';
+        } else {
+            $resultado.= '<tr class="r1">';
+        }
+        $resultado.= '<td>' . $row[nombre]. '</td>';
+        $resultado.= '<td>' . $row[monto]. '</td>';
+        $resultado.= '<td>' . $row[tipo]. '</td>';
+        $resultado.= '</tr>';
+        $color = !$color;
+    }
+    $resultado.= '</table>';
+    $resultado.= '</form>';
+    $objResponse->addAssign("ResultadoCa", "innerHTML", $resultado);
+    return $objResponse;
+}
+
+?>
