a7345ed4bf281600582416fa9c7c2984cff45c1e
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-05 21:25:29 +0000

    Gestion Tipo Servicio

---
+++ com.foo.makororeservas/eventos/AgenciasPorPagar.php
@@ -0,0 +1,6 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/ClienteAgencia.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorClienteAgenciaBD.class.php';
+
+
+?>
