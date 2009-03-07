b7820371badd45ee394ef970f94a406b76e40fa6
trujillo.jonathan <trujillo.jonathan@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-07 00:06:16 +0000

    GUI calculo de horas

--- trunk/com.foo.makororeservas/eventos/CalculoHorasTripulante.php
+++ trunk/com.foo.makororeservas/eventos/CalculoHorasTripulante.php
@@ -11,49 +11,69 @@ function calculoHora($datos) {
     $resultado = "";
     $controlLogica = new ControlTripulanteLogicaclass();
     $Coleccion = $controlLogica->consultarSueldoNominaTripulantesDetalles($fechaini, $fechafin);
-    
+
     foreach ($Coleccion as $variable) {
 
-    $recursoDetalles = $variable->getColeccionDetalles();
-    $total = $variable->getTotalPago();
-    $tripulante = $variable->getTripulante();
-    $totalGeneral += $total;
+        $recursoDetalles = $variable->getColeccionDetalles();
+        $total = $variable->getTotalPago();
+        $tripulante = $variable->getTripulante();
+        $totalGeneral += $total;
 
-    $resultado.= "\n";
-    $resultado.= "\n";
-    $resultado.= '<table border=1>';
-    $resultado.= '<tr>';
-    $resultado.= '<th>Cedula</th>';
-    $resultado.= '<th>Nombre</th>';
-    $resultado.= '<th>Apellido</th>';
-    $resultado.= '<th>Sitio Salida</th>';
-    $resultado.= '<th>Sitio Llegada</th>';
-    $resultado.= '<th>Tiempo</th>';
-    $resultado.= '<th>Matricula</th>';
-    $resultado.= '<th>Cargo</th>';
-    $resultado.= '<th>SUBTOTAL</th>';
-    $resultado.= '</tr>';
-    while ($row = mysql_fetch_array($recursoDetalles)) {
-        $controlTipoCargo = new controladorTipoCargoBDclass();
-        $tarifa = $controlTipoCargo->obtenerSueldoTipoCargo($row[idCargo]);
+
+        $resultado.= '<hr size="1" width="98%" color="#067AC2">';
+        $resultado.= '<div class="tituloNegro3">CI: ' . $tripulante->getCedula() . ' - ' . $tripulante->getApellido() . ', ' . $tripulante->getNombre() . '</div>';
+        $resultado.= '</table>';
+        $resultado.= '<table width="100%" class="formTable" cellspacing="0">';
+        $resultado.= '<thead>';
         $resultado.= '<tr>';
-        $resultado.= '<td>' . $row[cedula]. '</td>';
-        $resultado.= '<td>' . $row[nombre]. '</td>';
-        $resultado.= '<td>' . $row[apellido]. '</td>';
-        $resultado.= '<td>' . $row[sitioSalida]. '</td>';
-        $resultado.= '<td>' . $row[sitioLlegada] . '</td>';
-        $resultado.= '<td>' . $row[tiempo]. '</td>';
-        $resultado.= '<td>' . $row[AVION_matricula]. '</td>';
-        $resultado.= '<td>' . $row[cargo]. '</td>';
-        $resultado.= '<td>' . $row[tiempo]*$tarifa . ' BS'. '</td>';
+        $resultado.= '<th>SITIO SALIDA</th>';
+        $resultado.= '<th>SITIO LLEGADA</th>';
+        $resultado.= '<th>TIEMPO</th>';
+        $resultado.= '<th>MATRICULA</th>';
+        $resultado.= '<th>CARGO</th>';
+        $resultado.= '<th>SUBTOTAL</th>';
         $resultado.= '</tr>';
-    }
+        $resultado.= '</thead>';
+        $resultado.= '<tfoot>';
+        $resultado.= '<tr>';
+        $resultado.= '<td>&nbsp</td>';
+        $resultado.= '<td>&nbsp</td>';
+        $resultado.= '<td>&nbsp</td>';
+        $resultado.= '<td>&nbsp</td>';
+        $resultado.= '<td align="center">Total:</td>';
+        $resultado.= '<td align="right">'. 'Bs. ' . $total .'</td>';
+        $resultado.= '</tr>';
+        $resultado.= '</tfoot>';
+        $color = false;
+        while ($row = mysql_fetch_array($recursoDetalles)) {
+            $controlTipoCargo = new controladorTipoCargoBDclass();
+            $tarifa = $controlTipoCargo->obtenerSueldoTipoCargo($row[idCargo]);
+            if ($color){
+                $resultado.= '<tr class="r0">';
+            } else {
+                $resultado.= '<tr class="r1">';
+            }
+            $resultado.= '<td>' . $row[sitioSalida]. '</td>';
+            $resultado.= '<td>' . $row[sitioLlegada] . '</td>';
+            $resultado.= '<td>' . $row[tiempo]. '</td>';
+            $resultado.= '<td>' . $row[AVION_matricula]. '</td>';
+            $resultado.= '<td>' . $row[cargo]. '</td>';
+            $resultado.= '<td align="right">' . ' Bs. ' . $row[tiempo]*$tarifa . '</td>';
+            $resultado.= '</tr>';
+            $color = !$color;
+        }
+        $resultado.= '</table>';
+        $resultado.= '<br/>';
 
-    $resultado.= '</table>';
-    $resultado.= '<table border=1><tr><td> Total BS '. $total .'</td></tr>';
-    $resultado.= '</table>';
-    $resultadoTotal = '<table border=1><tr><td> Total Egresos '. $totalGeneral .'</td></tr></table>';
-}
+        $resultadoTotal = '<hr size="1" width="98%" color="#067AC2">';
+        $resultadoTotal.= '<table  width="100%" class="formTable" cellspacing="0">';
+        $resultadoTotal.= '<tfoot>';
+        $resultadoTotal.= '<tr>';
+        $resultadoTotal.= '<td align="right">'. 'Total Egresos: Bs. ' . $totalGeneral .'</td>';
+        $resultadoTotal.= '</tr>';
+        $resultadoTotal.= '</tfoot>';
+        $resultadoTotal.= '</table><br />';
+    }
     $objResponse->addAssign("ResultadoCa", "innerHTML", $resultado);
     $objResponse->addAssign("totalGeneral", "innerHTML", $resultadoTotal);
 
