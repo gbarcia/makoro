f76e105f96dff14b01082a79656947c715c28bfb
trujillo.jonathan <trujillo.jonathan@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-08 21:34:51 +0000

    reservas!

--- com.foo.makororeservas/eventos/GestionReserva.php
+++ com.foo.makororeservas/eventos/GestionReserva.php
@@ -34,7 +34,7 @@ function procesarFiltros($datos){
     $controlVuelo = new ControlVueloLogicaclass();
     $coleccionVuelo = $controlVuelo->vueloEspecificoConFiltro($datos[fechaInicio],
         $datos[fechaFin], '', '', $arregloRuta[0], $arregloRuta[1], $capacidad,
-        $dato[cedulaPasaportePasajero], $datos[nombrePasajero], $datos[apellidoPasajero],
+        $datos[cedulaPasaportePasajero], $datos[nombrePasajero], $datos[apellidoPasajero],
         $datos[cedulaCliente], $datos[nombreParticular], $datos[apellidoParticular],
         $datos[rifCliente], $datos[nombreAgencia], $datos[solicitud], $datos[estado]);
     $resultado = "";
@@ -63,7 +63,11 @@ function procesarFiltros($datos){
         $copiloto = $var->getCopiloto();
         $disponibilidad = $var->getDisponibilidad();
         $idVuelo = $var->getIdvuelo();
-        $resultado.= '<tr>';
+        if ($color){
+            $resultado.= '<tr class="r0">';
+        } else {
+            $resultado.= '<tr class="r1">';
+        }
         $resultado.= '<td>' . $recursoDetalles->getFecha(). '</td>';
         $resultado.= '<td>' . $recursoDetalles->getHora(). '</td>';
         $resultado.= '<td>' . $recursoDetalles->getRutaSitioSalida(). '</td>';
@@ -73,8 +77,66 @@ function procesarFiltros($datos){
         $resultado.= '<td>' . $recursoDetalles->getAvionMatricula(). '</td>';
         $resultado.= '<td>' . $piloto. '</td>';
         $resultado.= '<td>' . $copiloto. '</td>';
-        $resultado.= '<td><input type="button" value="EDITAR" onclick="xajax_editar('.$idVuelo.')"/></td>';
+        $resultado.= '<td><input type="button" value="VER" onclick="xajax_editar('.$idVuelo.')"/></td>';
         $resultado.= '</tr>';
+        $color = !$color;
+    }
+    $resultado.= '</table>';
+    $resultado.= '</form>';
+    if ($recursoDetalles == "") {
+        $resultado = 'No hay coincidencias con su busqueda.';
+    }
+    $objResponse->addAssign("gestionReserva", "innerHTML", "$resultado");
+    return $objResponse;
+}
+
+function inicio(){
+    $controlVuelo = new ControlVueloLogicaclass();
+    $coleccionVuelo = $controlVuelo->vueloEspecificoSinFiltro(date("Y")."-".date("m").'-'.date('d'),
+        date("Y")."-".date("m").'-'.date('d'));
+    $resultado = "";
+    $objResponse = new xajaxResponse();
+    $resultado = '<form id="formularioEditarMarcar">';
+    $resultado.= '<table class="scrollTable" cellspacing="0">';
+    $resultado.= '<thead>';
+    $resultado.= '<tr>';
+    $resultado.= '<th>FECHA</th>';
+    $resultado.= '<th>HORA</th>';
+    $resultado.= '<th>ORIGEN</th>';
+    $resultado.= '<th>DESTINO</th>';
+    $resultado.= '<th>ASIENTOS DISPONIBLES</th>';
+    $resultado.= '<th>DISPONIBILIDAD</th>';
+    $resultado.= '<th>AVION</th>';
+    $resultado.= '<th>PILOTO</th>';
+    $resultado.= '<th>COPILOTO</th>';
+    $resultado.= '<th>DETALLES</th>';
+    $resultado.= '</tr>';
+    $resultado.= '</thead>';
+    $color = false;
+    foreach ($coleccionVuelo as $var) {
+        $recursoDetalles = $var->getColeccionVuelo();
+        $cantidadDisponible = $var->getAsientosDisponibles();
+        $piloto = $var->getPiloto();
+        $copiloto = $var->getCopiloto();
+        $disponibilidad = $var->getDisponibilidad();
+        $idVuelo = $var->getIdvuelo();
+        if ($color){
+            $resultado.= '<tr class="r0">';
+        } else {
+            $resultado.= '<tr class="r1">';
+        }
+        $resultado.= '<td>' . $recursoDetalles->getFecha(). '</td>';
+        $resultado.= '<td>' . $recursoDetalles->getHora(). '</td>';
+        $resultado.= '<td>' . $recursoDetalles->getRutaSitioSalida(). '</td>';
+        $resultado.= '<td>' . $recursoDetalles->getRutaSitioLLegada(). '</td>';
+        $resultado.= '<td>' . $cantidadDisponible. '</td>';
+        $resultado.= '<td>' . $disponibilidad. '</td>';
+        $resultado.= '<td>' . $recursoDetalles->getAvionMatricula(). '</td>';
+        $resultado.= '<td>' . $piloto. '</td>';
+        $resultado.= '<td>' . $copiloto. '</td>';
+        $resultado.= '<td><input type="button" value="VER" onclick="xajax_editar('.$idVuelo.')"/></td>';
+        $resultado.= '</tr>';
+        $color = !$color;
     }
     $resultado.= '</table>';
     $resultado.= '</form>';
