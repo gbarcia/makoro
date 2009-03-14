5a905384ebe2cdd8d59e8198cbe0c43cf33d31d8
gerardobarciap <gerardobarciap@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-14 18:13:38 +0000

    arreglos de session

--- trunk/com.foo.makororeservas/eventos/CerrarSesion.php
+++ trunk/com.foo.makororeservas/eventos/CerrarSesion.php
@@ -7,6 +7,7 @@ $objResponse = new xajaxResponse();
 $objResponse->addConfirmCommands(3, "Confirma que desea salir del sistema?");
 $_SESSION = array();
 session_destroy();
-$objResponse->addRedirect($_SERVER['DOCUMENT_ROOT'] .'/com.foo.makororeservas/');
+$objResponse->addRedirect('../index.php');
+return $objResponse;
 }
 ?>
