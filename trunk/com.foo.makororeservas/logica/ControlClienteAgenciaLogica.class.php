0e447e9bcf8d9eafb1d34b68746f0c554f13d38a
mayita.uribe <mayita.uribe@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-17 21:45:00 +0000

    Gestion cliente agencia

--- trunk/com.foo.makororeservas/logica/ControlClienteAgenciaLogica.class.php
+++ trunk/com.foo.makororeservas/logica/ControlClienteAgenciaLogica.class.php
@@ -13,17 +13,17 @@ class ControlClienteAgenciaLogicaclass {
         $this->controlBD = new controladorClienteAgenciaBDclass();
     }
 
-/**
- * Metodo para agregar un nuevo cliente agencia
- * @param <String> $rif
- * @param <String> $nombre
- * @param <String> $telefono
- * @param <String> $direccion
- * @param <String> $estado
- * @param <String> $ciudad
- * @param <double> $porcentajeComision
- * @return <boolean> resultado de la operacion
- */
+    /**
+     * Metodo para agregar un nuevo cliente agencia
+     * @param <String> $rif
+     * @param <String> $nombre
+     * @param <String> $telefono
+     * @param <String> $direccion
+     * @param <String> $estado
+     * @param <String> $ciudad
+     * @param <double> $porcentajeComision
+     * @return <boolean> resultado de la operacion
+     */
     function nuevoClienteAgencia($rif, $nombre, $telefono, $direccion, $estado, $ciudad, $porcentajeComision){
         $clienteAgencia = new ClienteAgenciaclass();
         $clienteAgencia->setRif($rif);
@@ -38,17 +38,17 @@ class ControlClienteAgenciaLogicaclass {
         return ($resultado);
     }
 
-/**
- * Metodo para actualizar los datos del cliente agencia
- * @param <String> $rif
- * @param <String> $nombre
- * @param <String> $telefono
- * @param <String> $direccion
- * @param <String> $estado
- * @param <String> $ciudad
- * @param <String> $porcentajeComision
- * @return <boolean> resultado de la operacion
- */
+    /**
+     * Metodo para actualizar los datos del cliente agencia
+     * @param <String> $rif
+     * @param <String> $nombre
+     * @param <String> $telefono
+     * @param <String> $direccion
+     * @param <String> $estado
+     * @param <String> $ciudad
+     * @param <String> $porcentajeComision
+     * @return <boolean> resultado de la operacion
+     */
     function actualizarClienteAgencia($rif, $nombre, $telefono, $direccion, $estado, $ciudad, $porcentajeComision){
         $clienteAgencia = new ClienteAgenciaclass();
         $clienteAgencia->setRif($rif);
@@ -62,45 +62,45 @@ class ControlClienteAgenciaLogicaclass {
         return ($resultado);
     }
 
-/**
- * Metodo para consultar a un cliente agencia segun la busqueda
- * @param <String, Integer> $busqueda
- * @return <recurso> recurso de un objeto cliente agencia
- */
+    /**
+     * Metodo para consultar a un cliente agencia segun la busqueda
+     * @param <String, Integer> $busqueda
+     * @return <recurso> recurso de un objeto cliente agencia
+     */
     function busquedaClienteAgenciaRifNombre($busqueda){
         $recurso = false;
         $recurso = $this->controlBD->consultarClienteAgenciaRifNombre($busqueda);
         return $recurso;
     }
 
-/**
- * Metodo para consultar el cliente agencia con mas vuelos
- * @return <recurso> objeto cliente agencia
- */
+    /**
+     * Metodo para consultar el cliente agencia con mas vuelos
+     * @return <recurso> objeto cliente agencia
+     */
     function busquedaClienteAgenciaConMasVuelos(){
         $recurso = false;
         $recurso = $this->controlBD->consultarClienteAgenciaConMasVuelos();
         return $recurso;
     }
 
-/**
- * Metodo para consultar por orden descendente los clientes agencias segun la
- * cantidad de vuelos pagados
- * @return <Coleccion> coleccion de clientes agencias
- */
+    /**
+     * Metodo para consultar por orden descendente los clientes agencias segun la
+     * cantidad de vuelos pagados
+     * @return <Coleccion> Coleccion de clientes agencias 
+     */
     function busquedaClientesAgenciasVuelosDescendente(){
         $resultado = false;
         $resultado = $this->controlBD->consultarClientesAgenciasVuelosDescendente();
         return $resultado;
     }
 
-/**
- * Metodo para consultar los clientes agencias que faltan por pagar en
- * un lapso de tiempo determinado
- * @param <Date> $fechaInicio
- * @param <Date> $fechaFin
- * @return <Coleccion> coleccion de los clientes agencias 
- */
+    /**
+     * Metodo para consultar los clientes agencias que faltan por pagar en
+     * un lapso de tiempo determinado
+     * @param <Date> $fechaInicio La fecha de inicio a consultar
+     * @param <Date> $fechaFin La fecha de fin a consultar
+     * @return <Coleccion> Coleccion de los clientes agencias
+     */
     function busquedaClientesAgenciasPorPagar($fechaInicio, $fechaFin){
         $resultado = false;
         $resultado = $this->controlBD->consultarClientesAgenciasPorPagar($fechaInicio, $fechaFin);
