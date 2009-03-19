679358e528943e6427e5b074e25c79c6bfe58390
trujillo.jonathan <trujillo.jonathan@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-19 20:03:35 +0000

    GUI Reserva

--- trunk/com.foo.makororeservas/eventos/GestionReservaFormularios.php
+++ trunk/com.foo.makororeservas/eventos/GestionReservaFormularios.php
@@ -6,16 +6,20 @@ function detalles($idVuelo){
     $resultado.= '<table class="scrollTable" cellspacing="0">';
     $resultado.= '<thead>';
     $resultado.= '<tr>';
-    $resultado.= '<th>SOLICITUD</th>';
-    $resultado.= '<th>CI/PASAPORTE</th>';
-    $resultado.= '<th>NOMBRE</th>';
+    $resultado.= '<th>&nbsp</th>';
+    $resultado.= '<th>LOCALIZADOR</th>';
+    $resultado.= '<th>CI/PASS</th>';
+    $resultado.= '<th>PASAJERO</th>';
     $resultado.= '<th>TIPO</th>';
-    $resultado.= '<th>SERVICIO</th>';
+    $resultado.= '<th>SERV</th>';
     $resultado.= '<th>POSADA</th>';
     $resultado.= '<th>VENDEDOR</th>';
     $resultado.= '<th>SUCURSAL</th>';
     $resultado.= '<th>RETORNO</th>';
-    $resultado.= '<th>NOMBRE CLIENTE</th>';
+    $resultado.= '<th>CLIENTE</th>';
+    $resultado.= '<th>P</th>';
+    $resultado.= '<th>BANCO</th>';
+    $resultado.= '<th>TRANSACCION</th>';
     $resultado.= '</tr>';
     $resultado.= '</thead>';
     while ($row = mysql_fetch_array($recurso)){
@@ -26,16 +30,35 @@ function detalles($idVuelo){
         } else{
             $resultado.= '<tr class="r1">';
         }
+        $resultado.= '<td><input type="checkbox" name="" value="ON" /></td>';
         $resultado.= '<td>' . $row[solicitud] . '</td>';
-        $resultado.= '<td>' . $row[cedulaPasaporte] . '</td>';
+        if ($row[cedulaPasaporte] != ''){
+            $cedulaPasaporte = $row[cedulaPasaporte];
+        } else {
+            $cedulaPasaporte = '&nbsp';
+        }
+        $resultado.= '<td>' . $cedulaPasaporte . '</td>';
         $resultado.= '<td>' . $row[pasajero] . '</td>';
-        $resultado.= '<td>' . $row[tipoPasajero] . '</td>';
+        if ($row[cedulaPasaporte] != ''){
+            $tipoPasajero = $row[tipoPasajero];
+        } else {
+            $tipoPasajero = '&nbsp';
+        }
+        $resultado.= '<td>' . $tipoPasajero . '</td>';
         $resultado.= '<td>' . $row[servicio] . '</td>';
-        $resultado.= '<td>' . $row[posada] . '</td>';
+        if ($row[posada] != ''){
+            $posada = $row[posada];
+        } else {
+            $posada = '&nbsp';
+        }
+        $resultado.= '<td>' . $posada . '</td>';
         $resultado.= '<td>' . $row[encargadoNombre] . '</td>';
         $resultado.= '<td>' . $row[sucursal] . '</td>';
         $resultado.= '<td>' . $row[vueloRetorno] . '</td>';
         $resultado.= '<td>' . $row[clienteNombre] . '</td>';
+        $resultado.= '<td>' . $row[pago] . '</td>';
+        $resultado.= '<td>' . $row[banco] . '</td>';
+        $resultado.= '<td>' . $row[numeroTran] . '</td>';
         $resultado.= '</tr>';
     }
     return $resultado;
@@ -69,6 +92,32 @@ function generarFormularioNuevaReserva($idVuelo) {
             <td><input type="text" name="cedula" id="cedula"></td>
         </tr>
         <tr class="r1">
+            <td colspan="3">
+                <div align="center">
+                    <input name="button" type="button" id="button" value="BUSCAR CLIENTE" onclick= "xajax_buscarCliente(xajax.getFormValues(\'formNuevaReserva\'))">
+                </div>
+            </td>
+        </tr>
+    </table>
+<table class="formTable" cellspacing="0">
+        <tr>
+            <thead>
+                <td colspan="3">
+                    <div class="tituloBlanco1">
+                        RESERVA EXISTENTE
+                        <div class="botonCerrar">
+                            <button name="boton" type="button" onclick="xajax_cerrarVentanaEditar()" style="margin:0px; background-color:transparent; border:none;"><img src="iconos/cerrar.png" alt="x"/></button>
+                        </div>
+                    </div>
+                </td>
+            </thead>
+        </tr>
+        <tr class="r1">
+            <td><input type="radio" name="grupo" value="juridico" checked="checked" /></td>
+            <td>Localizador</td>
+            <td><input type="text" name="rif" id="rif"></td>
+        </tr>
+        <tr class="r0">
             <td colspan="3">
                 <div align="center">
                     <input name="button" type="button" id="button" value="BUSCAR CLIENTE" onclick= "xajax_buscarCliente(xajax.getFormValues(\'formNuevaReserva\'))">
@@ -220,7 +269,70 @@ function generarFormularioAgregarClienteJuridico($rif) {
 </table>
 </form>';
     return $contenido;
-    //    <textarea name="" rows="4" cols="20"></textarea>
+}
+
+function generarFormularioAgregarClienteParticular($cedula) {
+    $contenido = '<form id="formNuevoCliente">
+  <table class="formTable" cellspacing="0">
+    <tr>
+        <thead>
+        <td colspan="2">
+        <div class="tituloBlanco1">
+            NUEVO CLIENTE NATURAL
+            <div class="botonCerrar">
+            <button name="boton" type="button" onclick="xajax_cerrarVentana()" style="margin:0px; background-color:transparent; border:none;"><img src="iconos/cerrar.png" alt="x"/></button>
+        </div>
+        </div>
+        </td>
+        </thead>
+    </tr>
+    <tr class="r1">
+      <td colspan="2">(*) Son campos obligatorios</td>
+      </tr>
+    <tr class="r0">
+      <td>Cedula</td>
+      <td><label>
+        <input type="text" name="cedula" id="cedula" value="'.$cedula.'" readonly="readonly"/>
+      </label></td>
+    </tr>
+    <tr class="r1">
+      <td>(*) Nombre</td>
+      <td><label>
+        <input type="text" name="nombre" id="nombre" onkeyup="this.value=this.value.toUpperCase();" />
+      </label></td>
+    </tr>
+    <tr class="r0">
+      <td>(*) Apellido</td>
+      <td><label>
+        <input type="text" name="apellido" id="apellido" onkeyup="this.value=this.value.toUpperCase();" />
+      </label></td>
+    </tr>
+    <tr class="r1">
+      <td>(*) Telefono</td>
+      <td><input type="text" name="telefono" id="telefono" onkeyup="this.value=this.value.toUpperCase();" /></td>
+    </tr>
+    <tr class="r0">
+      <td>Estado</td>
+      <td><input type="text" name="estado" id="estado" onkeyup="this.value=this.value.toUpperCase();" /></td>
+    </tr>
+    <tr class="r1">
+      <td>Ciudad</td>
+      <td><input type="text" name="ciudad" id="ciudad" onkeyup="this.value=this.value.toUpperCase();" /></td>
+    </tr>
+    <tr class="r0">
+      <td>Direccion</td>
+      <td><textarea name="direccion" id="direccion" cols="23" onkeyup="this.value=this.value.toUpperCase();"></textarea></td>
+    </tr>
+    <tr class="r1">
+      <td height="26" colspan="2"><div align="center">
+        <input name="button" type="button" id="button" value="AGREGAR" onclick= "xajax_procesarCliente(xajax.getFormValues(\'formNuevoCliente\'))" />
+      </div></td>
+    </tr>
+  </table></td>
+    </tr>
+</table>
+</form>';
+    return $contenido;
 }
 
 ?>
