9127aec34446548eff1af01b398d2e5488fa8c96
dianitau <dianitau@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-20 23:10:16 +0000

    Equipaje

---
+++ trunk/com.foo.makororeservas/logica/ControlEquipajeLogica.class.php
@@ -0,0 +1,44 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/serviciotecnico/persistencia/controladorEquipajeBD.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/com.foo.makororeservas/dominio/Equipaje.class.php';
+/**
+ * Description of ControlEquipajeLogicaclass
+ * Clase para manejar la logica de los equipajes
+ * @author Diana Uribe
+ */
+class ControlEquipajeLogicaclass {
+    private $controlBD;
+
+    function __construct() {
+        $this->controlBD = new controladorEquipajeBDclass();
+    }
+
+/**
+ * Metodo para agregar un nuevo equipaje a la reserva a la cual pertenece
+ * @param <String> $descripcion
+ * @param <String> $tipo
+ * @param <Integer> $peso
+ * @param <Integer> $reservaId
+ * @return <boolean> resultado de la operación
+ */
+    function nuevoEquipajePasajero($descripcion, $tipo, $peso, $reservaId) {
+        $equipaje = new Equipajeclass();
+        $equipaje->setDescripcion($descripcion);
+        $equipaje->setTipo($tipo);
+        $equipaje->setPeso($peso);
+        $equipaje->setReservaId($reservaId);
+        $resultado = $this->controlBD->agregarEquipaje($equipaje);
+        return ($resultado);
+    }
+
+    function buscarEquipajePasajeroPorReserva($idReserva) {
+        $resultado = new ArrayObject();
+        $recurso = $this->controlBD->consultarEquipajePorPasajeroReserva($idReserva);
+        $row = mysql_fetch_array($recurso);
+            $descripcionEquipaje = $row[descripcion];
+     
+        
+        return $descripcionEquipaje;
+    }
+}
+?>
