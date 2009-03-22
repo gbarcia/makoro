c40b6fd90d7d00bb2d3d99bffee63c4668392ff2
mayita.uribe <mayita.uribe@6345bbea-f8a1-11dd-99c0-75d572ba1ddd>
2009-03-22 18:32:06 +0000

    Gestion Reserva

--- trunk/com.foo.makororeservas/test/TestLogica/TestControlLogicaReserva.php
+++ trunk/com.foo.makororeservas/test/TestLogica/TestControlLogicaReserva.php
@@ -70,7 +70,10 @@ $clienteTest = new ClienteParticularclass();
 //$resultado = $controlTest->actualizarPasajeroReserva('joniii', 'TRUJI-truji', 'M', 17706709, null, 'VENEZOLANO', 'ADL', 23);
 //$resultado = $controlTest->pagarReserva('45T5', 'E', 100, '', '', 1);
 //$resultado = $controlTest->actualizarEstadoReserva(11, 'PA');
-$resultado = $controlTest->actualizarEstadoReserva('ZMT5', 'PA', 'E', 50.0, '', '', 2);
+//$resultado = $controlTest->actualizarEstadoReserva('ZMT5', 'PA', 'E', 50.0, '', '', 2);
+//echo $resultado;
+
+$resultado = $controlTest->crearReserva('ida', 2, 2, 0, '2009-03-22', 1, 1, 111, 17706708, null, null, null, 'PP');
 echo $resultado;
 
 /*----------------------------*/
